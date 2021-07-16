f11d763331a4123d0674b5e0e37e679113d98f0c
dell-presalesacademy <presalesacademy@dell.com>
2021-07-16 09:07:47 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresalesWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PresalesAcademy()
+        {
+            return View();
+        }
+
+        public IActionResult InternshipProgram()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult InternshipHighlights()
+        {
+            return View();
+        }
+
+        public IActionResult InternTestimonials()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult RoleProgression()
+        {
+            return View();
+        }
+
+        public IActionResult NewHireTestimonials()
+        {
+            return View();
+        }
+
+        public IActionResult Learning()
+        {
+            return View();
+        }
+    }
+}
